f2c0a5802167a8be3fb28a61a9e9f4382ffc7526
tomual <tomual@users.noreply.github.com>
2019-02-25 17:37:56 +0800

    Default config set

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php
 
-define("FOLDER_NAME", "4pm");
-define("UPLOAD_FOLDER", "../uploads");
+define("UPLOAD_FOLDER", "uploads");
 define("PUBLIC_URL", "http://localhost/uploads");
--- index.php
+++ index.php
@@ -23,12 +23,11 @@ if (isset($_FILES['upload']['name'])) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>4pm File Uploader & Renamer</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-
 <div class="container">
 	<div class="row">
 		<div class="col text-center py-5 mb-5">
@@ -45,7 +44,5 @@ if (isset($_FILES['upload']['name'])) {
 		</div>
 	</div>
 </div>
-
-
 </body>
 </html>
